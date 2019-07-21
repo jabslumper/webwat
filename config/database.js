8167b31425e8b679caa7a4012ae716a565d7b214
Chawalit Puanlong <jabslumper@gmail.com>
2019-07-21 16:21:55 +0700

    test

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   multipleStatements: true,
   host: "localhost",
   user: "root",
-  password: "",
+  password: "8qoLl0YA1oTE1w0j",
   database: "webwat"
 });
 export default connection;
